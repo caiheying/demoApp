fc71982d1cf8aa34d9325f560f36f51f1014c3b2
bowclerk <hcai665@aucklanduni.ac.nz>
2018-12-12 23:39:49 +1300

    Init User LoginUI

---
+++ tabbedApp/tabbedApp/tabbedApp/Models/User.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace tabbedApp.Models
+{
+    public class User
+    {
+        //[PrimaryKey]
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+
+        public User() { }
+
+        public User(string Username, string Password)
+        {
+            //Console.WriteLine($"...{Username}      {Password}");
+            this.Username = Username;
+            this.Password = Password;
+        }
+
+        public bool CheckInfomation()
+        {
+            if (string.IsNullOrEmpty(this.Username) || string.IsNullOrEmpty(this.Password))
+                return false;
+            else
+                return true;
+        }
+    }
+}
---
+++ tabbedApp/tabbedApp/tabbedApp/Views/LoginPage.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using tabbedApp.Models;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace tabbedApp.Views
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class LoginPage : ContentPage
+	{
+        public LoginPage()
+        {
+            InitializeComponent();
+            Init();
+        }
+
+        void Init()
+        {
+            BackgroundColor = Constants.BackgroundColor;
+            Lbl_Username.TextColor = Constants.MainTextColor;
+            Lbl_Password.TextColor = Constants.MainTextColor;
+            ActivitySpinner.IsVisible = false;
+            LoginIcon.HeightRequest = Constants.LoginIconHeight;
+        }
+
+        void SignInProcedure(object sender, EventArgs e)
+        {
+            User user = new User(Entry_Username.Text, Entry_Password.Text);
+            if (user.CheckInfomation())
+            {
+                DisplayAlert("Login", "Login Success", "Oke");
+
+            }
+            else
+            {
+                DisplayAlert("Login", "Login Fail", "Oke");
+            }
+        }
+    }
+}
