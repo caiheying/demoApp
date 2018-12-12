fe487626b522fdbfb709e7aaf97e646d902947b3
bowclerk <hcai665@aucklanduni.ac.nz>
2018-12-12 16:48:33 +1300

    Tabbed App Init

---
+++ demoApp/demoApp/demoApp/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using demoApp.Models;
+
+namespace demoApp.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
